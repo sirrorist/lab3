e2d37634ddede046e27b29bc52b387f00f515027
sirrorist <64726393+sirrorist@users.noreply.github.com>
2022-11-14 22:57:14 +0500

    update

--- lab4.py
+++ lab4.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/KoichiYasuoka/bert-base-russian-upos"
@@ -6,9 +7,12 @@ headers = {"Authorization": "Bearer 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"}
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
-	
+
+st.write("# Real Time Sentiment Analysis")
+user_input = st.text_input("Please rate our services >>: ")
+
 output = query({
-	"inputs": str(input())
+	"inputs": str(user_input)
 })
 i = 0
 for o in output:
