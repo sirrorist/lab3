4f97e63d22a450941c0e0aea5ee5ac25581b6804
stud0000212941 <stud0000212941@study.utmn.ru>
2023-02-18 00:28:49 +0500

    nu 4to PI

--- test_task2.py
+++ test_task2.py
@@ -1,10 +1,12 @@
-from fastapi import testclient
+from fastapi.testclient import TestClient
 from task2 import app
 
-client = testclient(app)
+client = TestClient(app)
 
 
 def test_read_task2():
-    response=client.post("/predict/", json={"text":"Моё имя Рустам и я из Пакистана"}) 
+    response=client.post("/predict/", json={"text":"Я Артём живу в Питере"}) 
+    json_data = response.json()
+    json_data1=json_data[1]
     assert response.status_code == 200 
-    # assert json_data["entity_group"] == "DET"
+    assert json_data1["entity_group"] == "PROPN"
