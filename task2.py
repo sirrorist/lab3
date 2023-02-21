4cb5b4f44bf07c8f93a427d77332aed1196e043d
stud0000212941 <stud0000212941@study.utmn.ru>
2023-02-21 18:05:02 +0500

    Proverka

--- task2.py
+++ task2.py
@@ -2,20 +2,26 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 
+
 class Item(BaseModel):
-  text: str 
-  
-API_URL = "https://api-inference.huggingface.co/models/KoichiYasuoka/bert-base-russian-upos"
+    text: str
+
+
+API_URL = (
+  "https://api-inference.huggingface.co/models/KoichiYasuoka/bert-base-russian-upos"
+)
 headers = {"Authorization": "Bearer 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"}
 
-app=FastAPI()
+
+app = FastAPI()
+
 
 @app.get("/")
 def root():
-  return {"message": "Hello World"}
+    return {"message": "Hello World"}
+
 
 @app.post("/predict/")
 def query(payload: Item):
-	response = requests.post(API_URL, headers=headers, json=payload.text)
-	return response.json()
-  
+    response = requests.post(API_URL, headers=headers, json=payload.text)
+    return response.json()
