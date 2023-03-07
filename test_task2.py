95dd7c29e9a5c7407b5872efcdd9e54304f21066
stud0000212941 <116103111+stud0000212941@users.noreply.github.com>
2023-03-07 17:39:52 +0500

    test

--- test_task2.py
+++ test_task2.py
@@ -8,6 +8,6 @@ client = TestClient(app)
 def test_read_task2():
     response = client.post("/predict/", json={"text": "Я Артём живу в Питере"})
     json_data = response.json()
-    json_data1 = json_data[1]
+    json_data1 = json_data[0]
     assert response.status_code == 200
     assert json_data1["entity_group"] == "PROPN"
